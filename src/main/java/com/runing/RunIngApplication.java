90e3bff12d658b3cfe618b00f4c856c520d98ea1
손동혁 <sond5510@naver.com>
2025-12-18 14:06:24 +0900

    Chore : Initial project setup

---
+++ src/main/java/com/runing/RunIngApplication.java
@@ -0,0 +1,13 @@
+package com.runing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunIngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunIngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/runing/RunIngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunIngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
