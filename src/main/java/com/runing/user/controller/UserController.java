e5ebb03d55715424b1f84751ecef81a1b9a38483
손동혁 <sond5510@naver.com>
2025-12-23 15:58:06 +0900

    Feat : 회원가입 ì» API 컨트롤러 추가

---
+++ src/main/java/com/runing/user/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.runing.user.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.runing.user.dto.UserCreateRequest;
+import com.runing.user.dto.UserDto;
+import com.runing.user.service.UserService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/api/users")
+@Slf4j
+@RequiredArgsConstructor
+public class UserController {
+	private final UserService userService;
+
+	// 회원가입
+	@PostMapping
+	public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserCreateRequest request) {
+		UserDto userDto = userService.createUser(request);
+		return ResponseEntity.status(HttpStatus.CREATED)
+			.body(userDto);
+	}
+}
