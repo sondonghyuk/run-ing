7fc430f68d3f63795a5d55ecd0b82b000dfe8912
손동혁 <sond5510@naver.com>
2025-12-23 14:35:08 +0900

    Refactor : UserService

--- src/main/java/com/runing/user/service/UserService.java
+++ src/main/java/com/runing/user/service/UserService.java
@@ -1,6 +1,8 @@
 package com.runing.user.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.runing.common.error.BaseException;
 import com.runing.common.error.UserErrorCode;
@@ -10,35 +12,47 @@ import com.runing.user.entity.User;
 import com.runing.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserService {
 	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
 
 	// 유저 생성
-	public UserDto createUser(UserCreateRequest request){
+	@Transactional
+	public UserDto createUser(UserCreateRequest request) {
+
+		// 1. 변수 설정
 		String email = request.email();
 		String nickname = request.nickname();
+		String username = request.name();
+		String password = request.password();
+		String phoneNumber = request.phoneNumber();
 
-		// 1. 중복검증
-		if(userRepository.existsByEmail(email)){
+		log.debug("사용자 생성 시작: email={}, nickname={}", email, nickname);
+
+		// 2. 중복검증
+		if (userRepository.existsByEmail(email)) {
 			throw new BaseException(UserErrorCode.USER_EMAIL_EXISTS);
 		}
-		if(userRepository.existsByNickname(nickname)){
+		if (userRepository.existsByNickname(nickname)) {
 			throw new BaseException(UserErrorCode.USER_NICKNAME_EXISTS);
 		}
 
-		// 2. 유저 생성
-		User user = new User(request.email(),request.password(),request.name(),request.nickname(),request.phoneNumber(),request.profileUrl());
+		// 3. 유저 생성
+		// todo : profileURL -> BinaryContent
+		String hashedPassword = passwordEncoder.encode(password);
+		User user = new User(email, hashedPassword, username, nickname, phoneNumber, request.profileUrl());
 
-		// 3. 유저 저장
+		// 4. 유저 저장
 		userRepository.save(user);
+		log.info("사용자 생성 완료 : id={} , email={}, username={}", user.getId(), email, username);
 
-		// 4. userDto 변환
-		UserDto userDto = userMapper.toDto(user);
-
-		return userDto;
+		// 4. userDto 변환 후 반환
+		return userMapper.toDto(user);
 	}
 
 }
