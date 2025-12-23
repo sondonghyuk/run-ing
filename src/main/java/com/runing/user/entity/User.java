bf5205d246bc4e8401f9c488e0dfc43e98cb9d72
손동혁 <sond5510@naver.com>
2025-12-23 14:18:14 +0900

    Refactor : User Entity

--- src/main/java/com/runing/user/entity/User.java
+++ src/main/java/com/runing/user/entity/User.java
@@ -6,39 +6,41 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name="users")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseEntity {
 
 	@Column(nullable = false, unique = true, length = 255)
-	private String email;
+	private String email; // 이메일(아이디)
 
-	@Column(nullable = false)
-	private String password;
+	@Column(nullable = false, length = 255)
+	private String password; // 비밀번호
 
-	@Column(nullable = false, length = 20)
-	private String name;
+	@Column(nullable = false, length = 50)
+	private String name; // 실제 이름
 
 	@Column(nullable = false, unique = true, length = 20)
-	private String nickname;
+	private String nickname; // 사용할 닉네임
 
-	@Column(name = "phone_number", nullable = false, length = 20)
-	private String phoneNumber;
+	@Column(nullable = false, length = 20)
+	private String phoneNumber; // 전화번호(010-0000-1111)
 
-	@Column(name = "profile_url")
-	private String profileUrl;
+	@Column(length = 255)
+	private String profileUrl; // 프로필 이미지 url
 
 	@Enumerated(EnumType.STRING)
 	@Column(nullable = false)
 	private Role role;
 
-	@Column(name = "is_deleted", nullable = false)
-	private boolean isDeleted = false;
+	@Column(nullable = false)
+	private boolean deleted;
 
 	public User(String email, String password, String name, String nickname, String phoneNumber, String profileUrl) {
 		this.email = email;
@@ -48,27 +50,28 @@ public class User extends BaseEntity {
 		this.phoneNumber = phoneNumber;
 		this.profileUrl = profileUrl;
 		this.role = Role.USER;
+		this.deleted = false;
 	}
 
-	public void changeUsername(String newUsername) {
-		this.name = newUsername;
+	public void updateProfile(String newName, String newPhoneNumber, String newProfileUrl) {
+		if (newName != null && !newName.equals(this.name)){
+			this.name = newName;
+		}
+		if (newPhoneNumber != null && !newPhoneNumber.equals(this.phoneNumber)){
+			this.phoneNumber = newPhoneNumber;
+		}
+		if (newProfileUrl != null && !newProfileUrl.equals(this.profileUrl)){
+			this.profileUrl = newProfileUrl;
+		}
 	}
-	public void changeUserNickname(String newNickname) {
-		this.nickname = newNickname;
+
+	/**
+	 * 소프트 삭제
+	 */
+	public void delete() {
+		this.deleted = true;
 	}
-	public void changePassword(String encodePassword) {
-		this.password = encodePassword;
-	}
-	public void changePhoneNumber(String newPhoneNumber) {
-		this.phoneNumber = newPhoneNumber;
-	}
-	public void changeProfileUrl(String newProfileUrl) {
-		this.profileUrl = newProfileUrl;
-	}
-	public void changeRole(Role newRole) {
-		this.role = newRole;
-	}
-	public void changeIsDeleted(boolean isDeleted) {
-		this.isDeleted = isDeleted;
+	public void restore() {
+		this.deleted = false;
 	}
 }
