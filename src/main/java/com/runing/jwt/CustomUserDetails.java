19cd679d57c170d4999ec53cd7e7fcd4490a6d49
손동혁 <sond5510@naver.com>
2025-12-31 15:42:53 +0900

    Feat : CustomUserDetails 구현

---
+++ src/main/java/com/runing/jwt/CustomUserDetails.java
@@ -0,0 +1,60 @@
+package com.runing.jwt;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.runing.user.entity.User;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+	private final User user;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+		authorities.add(new GrantedAuthority() {
+			@Override
+			public @Nullable String getAuthority() {
+				return user.getRole().toString();
+			}
+		});
+		return authorities;
+	}
+
+	@Override
+	public @Nullable String getPassword() {
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return user.getName();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+}
--- src/main/java/com/runing/jwt/CustomUserDetailsService.java
+++ src/main/java/com/runing/jwt/CustomUserDetailsService.java
@@ -2,10 +2,9 @@ package com.runing.jwt;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.runing.common.error.BaseException;
-import com.runing.common.error.UserErrorCode;
 import com.runing.user.entity.User;
 import com.runing.user.repository.UserRepository;
 
@@ -20,12 +19,12 @@ public class CustomUserDetailsService implements UserDetailsService {
 	private final UserRepository userRepository;
 
 	@Override
-	public UserDetails loadUserByUsername(String username) {
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
 		User user = userRepository.findByUsername(username)
 			.orElseThrow(() -> {
 				log.warn("사용자를 찾을 수 없습니다 - username: {}", username);
-				return new BaseException(UserErrorCode.USER_NOT_FOUND); // 커스텀 예외
+				return new UsernameNotFoundException("사용자를 찾을 수 없습니다. - username : " +username);
 			});
 		log.debug("사용자 로드 성공 - username: {}", username);
 
