d09e1363f0816d03e4b97d59e243efa64fb72414
손동혁 <sond5510@naver.com>
2026-01-11 15:51:10 +0900

    Feat : AuthProvider 설명 추가

--- src/main/java/com/runing/user/entity/AuthProvider.java
+++ src/main/java/com/runing/user/entity/AuthProvider.java
@@ -1,7 +1,14 @@
 package com.runing.user.entity;
 
 public enum AuthProvider {
-	LOCAL,
-	KAKAO,
-	GOOGLE
+	LOCAL("일반 가입"),
+	KAKAO("카카오"),
+	GOOGLE("구글"),
+	NAVER("네이버");
+
+	private final String description;
+
+	AuthProvider(String description) {
+		this.description = description;
+	}
 }
