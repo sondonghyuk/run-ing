c246053216d87c8e7f40eb988146bc2f3e7117d4
손동혁 <sond5510@naver.com>
2026-01-14 15:04:59 +0900

    Refactor : Add transactional to NaverOAuth2UserService

--- src/main/java/com/runing/oauth/service/NaverOAuth2UserService.java
+++ src/main/java/com/runing/oauth/service/NaverOAuth2UserService.java
@@ -8,6 +8,7 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.runing.jwt.dto.CustomUserDetails;
 import com.runing.user.entity.AuthProvider;
@@ -19,6 +20,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class NaverOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
 	private final UserRepository userRepository;
