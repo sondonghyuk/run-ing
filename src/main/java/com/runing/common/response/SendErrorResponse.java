da3140d9192f0bde00b8c2f66bbfe686f06ce26e
손동혁 <sond5510@naver.com>
2026-01-15 15:17:19 +0900

    Refactor : Add objectMapper to SendErrorResponse

--- src/main/java/com/runing/common/response/SendErrorResponse.java
+++ src/main/java/com/runing/common/response/SendErrorResponse.java
@@ -1,15 +1,20 @@
 package com.runing.common.response;
 
 import java.io.IOException;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.http.HttpServletResponse;
 
 public class SendErrorResponse {
 
+	private static final ObjectMapper objectMapper = new ObjectMapper();
+
 	public static void sendResponse(HttpServletResponse response, String message) throws IOException {
 		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 		response.setContentType("application/json");
 		response.setCharacterEncoding("UTF-8");
-		response.getWriter().write("{\"error\": \"" + message + "\"}");
+		response.getWriter().write(objectMapper.writeValueAsString(Map.of("error",message)));
 	}
 }
